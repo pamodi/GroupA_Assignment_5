a7abecf33cb9fda2d4c8d43e72d4cf899348493d
Pamodi <pamo9055@gmail.com>
2024-04-02 23:20:13 -0400

    Add database setup function

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = ""
+	password = ""
+	dbname   = "user_management_db"
+)
+
+func main() {
+	// Setup database
+	db := SetupDatabase()
+	defer db.Close()
+}
+
+// Setup database
+func SetupDatabase() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the database")
+	return db
+}
