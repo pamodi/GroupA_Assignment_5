b3ee06987c26c161eaea60aa39e6f1c603a153b5
Pamodi <pamo9055@gmail.com>
2024-04-02 23:39:06 -0400

    Generate API token for more secure authentication

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GroupA_Assignment_5
 
 go 1.21.6
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 const (
@@ -14,10 +19,32 @@ const (
 	dbname   = "user_management_db"
 )
 
+// JWT Key
+var jwtKey = []byte("A7F077BEE149BB313832C810E5547FB647102356F375795FFB694CFB1A43E37B")
+
+// Struct to represent claims
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+// Struct to represent token
+type Token struct {
+	Token    string
+	ExpireAt time.Time
+}
+
 func main() {
 	// Setup database
 	db := SetupDatabase()
 	defer db.Close()
+
+	// Define API endpoints
+	http.HandleFunc("/token", GenerateTokenHandler())
+
+	fmt.Println("Server started on :8012")
+	// Start HTTP server
+	log.Fatal(http.ListenAndServe(":8012", nil))
 }
 
 // Setup database
@@ -36,4 +63,49 @@ func SetupDatabase() *sql.DB {
 
 	fmt.Println("Successfully connected to the database")
 	return db
+}
+
+// Generate token handler
+func GenerateTokenHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get user email from request header
+		formValue := r.PostFormValue("Email")
+		if formValue == "" {
+			http.Error(w, "Email header not found", http.StatusUnauthorized)
+			return
+		}
+
+		// Generate JWT token
+		tokenString, err := GenerateJWTToken(formValue)
+		if err != nil {
+			http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+			return
+		}
+
+		// Respond with JWT token
+		response := Token{Token: tokenString, ExpireAt: time.Now().Add(2 * time.Hour)}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+// Generate JWT token
+func GenerateJWTToken(email string) (string, error) {
+	// Create JWT token claims
+	claims := jwt.MapClaims{
+		"email":  email,
+		"expiry": time.Now().Add(2 * time.Hour).Unix(), // Token expires in 2 minutes
+	}
+
+	// Create JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign token with secret key
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	return tokenString, nil
 }
